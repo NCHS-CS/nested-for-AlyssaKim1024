695f8578127cdd3c64bdfa9a02071cc641ac9c18
AlyssaKim1024 <1117106@apps.nsd.org>
2024-10-08 19:17:32 +0000

    finished

--- src/main/java/org/nhs/app/App.java
+++ src/main/java/org/nhs/app/App.java
@@ -1,8 +1,84 @@
-/*----------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See LICENSE in the project root for license information.
- *---------------------------------------------------------------------------------------*/
+public class App
+{
+    public static void main(String args[])
+    {
+        practice1();
+        System.out.println();
+        practice2();
+        System.out.println();
+        practice3();
+    }
 
-package org.nhs.app;
+    public static void practice1()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
 
-// Your code goes here
+    public static void practice2()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 4; j >= i; j--)
+            {
+                System.out.print(".");
+            }
+            for (int k = 1; k <= i; k++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void practice3()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 4; j >= i; j--)
+            {
+               System.out.print(".");
+            }
+            System.out.print(i);
+            for (int k = 1; k <= i; k++)
+            {
+                System.out.print(".");
+            }
+            System.out.println();
+        }
+    }
+
+
+    /*
+    public static void practice()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= 4; j++)
+            {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+    */
+    
+    /*
+    public static void mystery()
+    {
+        int foo = 3;
+        for (int i = 0; foo > i; i += 2)
+        {
+            foo++;
+            System.out.println(foo + " " + i + " ");
+        }
+    }
+    */
+}
